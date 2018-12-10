fb100e12f0fbef711e23798274a96833a64d36c4
David Porter <david@claimdocs.com>
2018-12-09 22:00:33 -0600

    added scatter plot for housing

---
+++ public/javascripts/scatterplot.js
@@ -0,0 +1,59 @@
+(function(){
+  // Set the dimensions of the canvas / graph
+  var margin = {top: 30, right: 20, bottom: 30, left: 50},
+      width = 600 - margin.left - margin.right,
+      height = 600 - margin.top - margin.bottom;
+  // Parse the date / time
+  // var parseDate = d3.time.format("%d-%b-%y").parse;
+  // Set the ranges
+  var x = d3.scale.linear().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
+  // Define the axes
+  var xAxis = d3.svg.axis().scale(x)
+      .orient("bottom").ticks(5);
+  var yAxis = d3.svg.axis().scale(y)
+      .orient("left").ticks(5);
+  // Define the line
+  // var valueline = d3.svg.line()
+  //     .x(function(d) { return x(d.date); })
+  //     .y(function(d) { return y(d.close); });
+
+  // Adds the svg canvas
+  var svg = d3.select("#housing_scat")
+      .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+  // Get the data
+  d3.csv("viz_data/housing_scatter.csv", function(error, data) {
+      data.forEach(function(d) {
+          d.predict= +d.predicted;
+          d.act = +d.actual;
+      });
+      // Scale the range of the data
+      x.domain([0, d3.max(data, function(d) { return d.predict; })]);
+      y.domain([0, d3.max(data, function(d) { return d.act; })]);
+      // Add the valueline path.
+      // svg.append("path")
+      //     .attr("class", "line")
+      //     .attr("d", valueline(data));
+      // Add the scatterplot
+      svg.selectAll("dot")
+          .data(data)
+        .enter().append("circle")
+          .attr("r", 3.5)
+          .attr("cx", function(d) { return x(d.predict); })
+          .attr("cy", function(d) { return y(d.act); });
+      // Add the X Axis
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+      // Add the Y Axis
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
+  });
+})();
